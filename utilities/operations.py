dceec1491f001f7893e2b10e4db3c65faf64a89e
Gianmarco Ercolani <gianmarcoercolani94@gmail.com>
2025-07-12 01:09:54 +0200

    margins of right graphs

--- utilities/operations.py
+++ utilities/operations.py
@@ -53,33 +53,7 @@ def top_worst_graph(is_top, stocks, color, graph_title):
     else:
         max_value = stocks["earning"].min()
         graph_range = [max_value * 1.2, 0]
-    # fig = px.bar(
-    #     stocks,
-    #     x='label',
-    #     y='earning',
-    #     text='earning',
-    #     color_discrete_sequence=[color],
-    #     title=graph_title
-    # )
-    #
-    # fig.update_traces(
-    #                   texttemplate='€ %{text:.2f}',
-    #                   marker_line_width=0,  # remove white border
-    #                   marker_line_color='rgba(0,0,0,0)',
-    #                   textposition='outside',
-    #                   width=0.4,
-    #                   )
-    #
-    # fig.update_layout(
-    #                   xaxis_title='',
-    #                   yaxis_title='',
-    #                   height=280,
-    #                   margin=dict(t=60),
-    #                   showlegend=False,
-    #                   xaxis=dict(showgrid=False),
-    #                   yaxis=dict(showgrid=False, range=[graph_range[0], graph_range[1]]),
-    #                   )
-    # fig.update_yaxes(showticklabels=False, showgrid=False, zeroline=False)
+
     fig = go.Figure()
 
     # Add bar trace with modern styling
@@ -120,8 +94,9 @@ def top_worst_graph(is_top, stocks, color, graph_title):
         plot_bgcolor='#1E1E1E',
         paper_bgcolor='#1E1E1E',
         font=dict(family='Arial', color='#1f2937'),
-        margin=dict(l=50, r=50, t=60, b=50),
+        margin=dict(l=40, r=40, t=60, b=50),
         height=300,
+        width=300,
         showlegend=False
     )
     return fig
